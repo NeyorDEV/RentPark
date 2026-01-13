999fe050ff948c4d4db574e777699d9ec96bf7f5
mylan.perrier <mylan.perrier@etu.uca.fr>
2026-01-13 16:38:39 +0100

    test

--- Blazor/BlazorAdminRentPark/Program.cs
+++ Blazor/BlazorAdminRentPark/Program.cs
@@ -1,9 +1,15 @@
 using BlazorAdminRentPark.Components;
-using MudBlazor.Services;
 using BlazorAdminRentPark.Services;
+<<<<<<< HEAD
 using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Authentication.Cookies;
+=======
+using Microsoft.AspNetCore.Localization;
+using Microsoft.Extensions.Options;
+using MudBlazor.Services;
+using System.Globalization;
+>>>>>>> 5549b8f (add language selector not the resources)
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,6 +24,7 @@ builder.Services.AddScoped<IClientService, ClientService>();
 builder.Services.AddScoped<IVehiculeService, VehiculeService>();
 builder.Services.AddScoped<IUserService, UserService>();
 
+<<<<<<< HEAD
 builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddAuthorizationCore();
 builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
@@ -28,6 +35,19 @@ builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationSc
         options.LoginPath = "/login";
         options.ExpireTimeSpan = TimeSpan.FromMinutes(20);
     });
+=======
+builder.Services.AddControllers();
+
+builder.Services.AddLocalization(opts => { opts.ResourcesPath = "Resources"; });
+
+builder.Services.Configure<RequestLocalizationOptions>(options =>
+{
+    options.DefaultRequestCulture = new RequestCulture(new CultureInfo("fr-FR"));
+
+    options.SupportedCultures = new List<CultureInfo> { new CultureInfo("fr-FR"), new CultureInfo("en-US") };
+    options.SupportedUICultures = new List<CultureInfo> { new CultureInfo("fr-FR"), new CultureInfo("en-US") };
+});
+>>>>>>> 5549b8f (add language selector not the resources)
 
 var app = builder.Build();
 
@@ -39,6 +59,18 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+<<<<<<< HEAD
+=======
+var options = ((IApplicationBuilder)app).ApplicationServices.GetService<IOptions<RequestLocalizationOptions>>();
+
+if (options?.Value != null)
+{
+    app.UseRequestLocalization(options.Value);
+}
+
+app.MapControllers();
+
+>>>>>>> 5549b8f (add language selector not the resources)
 app.UseAntiforgery();
 
 app.MapStaticAssets();
