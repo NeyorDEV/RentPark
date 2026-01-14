b3e15cdf2ec1dd6fa31b04981445e24ee6594fa7
clement roudier <clement.roudier@etu.uca.fr>
2026-01-14 14:37:44 +0100

    Update a user

--- Blazor/BlazorAdminRentPark/Components/Pages/AddUserDialog.razor.cs
+++ Blazor/BlazorAdminRentPark/Components/Pages/AddUserDialog.razor.cs
@@ -1,5 +1,6 @@
 using BlazorAdminRentPark.Services;
 using BlazorAdminRentPark.UiModels;
+using BlazorAdminRentPark.Models;
 using Microsoft.AspNetCore.Components;
 using MudBlazor;
 
@@ -9,7 +10,9 @@ public partial class AddUserDialog
 {
     [Inject]
     private IUserService UserService { get; set; } = default!;
+    [Parameter] public UserModel? UserToEdit { get; set; }
 
+    private bool IsEditMode => UserToEdit != null;
     [CascadingParameter]
     private IMudDialogInstance MudDialog { get; set; } = default!;
 
@@ -17,13 +20,37 @@ public partial class AddUserDialog
     private bool _isValid;
     private UserUiModel _userModel = new();
 
+    protected override void OnInitialized()
+    {
+        if (IsEditMode)
+        {
+            // En mode édition, on pré-remplit le formulaire
+            // Note : On ne remplit pas le mot de passe car il est optionnel en modification
+            _userModel = new UserUiModel
+            {
+                Username = UserToEdit.Username,
+                Role = UserToEdit.Role
+            };
+        }
+    }
+
     private async Task HandleSubmit()
     {
         await _form.Validate();
-        
+
         if (_isValid)
         {
-            await UserService.Add(_userModel);
+            if (IsEditMode)
+            {
+                // Appel à la méthode Update avec l'ID d'origine
+                await UserService.Update(UserToEdit!.Id, _userModel);
+            }
+            else
+            {
+                // Comportement standard d'ajout
+                await UserService.Add(_userModel);
+            }
+
             MudDialog.Close(DialogResult.Ok(true));
         }
     }
