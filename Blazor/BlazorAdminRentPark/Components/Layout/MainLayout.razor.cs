2b5f66a9c090524b860da9a43bec4bf2ed89a9bf
clement roudier <clement.roudier@etu.uca.fr>
2026-01-15 08:12:21 +0100

    separer code des vues

---
+++ Blazor/BlazorAdminRentPark/Components/Layout/MainLayout.razor.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.Extensions.Localization;
+using MudBlazor;
+
+namespace BlazorAdminRentPark.Components.Layout
+{
+    public partial class MainLayout
+    {
+
+        [Inject]
+        protected NavigationManager NavManager { get; set; } = default!;
+
+        [Inject]
+        public IStringLocalizer<MainLayout> Localizer { get; set; } = default!;
+
+        private bool _drawerOpen = true;
+
+        protected void DrawerToggle()
+        {
+            _drawerOpen = !_drawerOpen;
+        }
+
+        protected void Logout()
+        {
+            NavManager.NavigateTo("/Account/Logout", forceLoad: true);
+        }
+    }
+}
+
