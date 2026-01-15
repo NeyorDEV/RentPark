b2836564288699878ca48008906d6f78e6918b0f
clement roudier <clement.roudier@etu.uca.fr>
2026-01-15 08:29:31 +0100

    séparer le code des vues navMenu

---
+++ Blazor/BlazorAdminRentPark/Components/Layout/NavMenu.razor.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Localization;
+
+namespace BlazorAdminRentPark.Components.Layout
+{
+    public partial class NavMenu
+    {
+        [Inject]
+        public IStringLocalizer<NavMenu> Localizer { get; set;} = default!;
+    }
+}
