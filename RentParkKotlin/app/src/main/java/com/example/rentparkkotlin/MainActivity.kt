350fb1458ac7c86e12b8987f5d8e8ca4347964df
clement roudier <clement.roudier@etu.uca.fr>
2026-02-27 08:40:33 +0100

    create view reservation

--- RentParkKotlin/app/src/main/java/com/example/rentparkkotlin/MainActivity.kt
+++ RentParkKotlin/app/src/main/java/com/example/rentparkkotlin/MainActivity.kt
@@ -16,6 +16,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.rentparkkotlin.ui.carsPage.CarListScreen
 import com.example.rentparkkotlin.ui.homeCustomer.RentParkHomeScreen
+import com.example.rentparkkotlin.ui.reservationsPage.ContratsScreen
 import com.example.rentparkkotlin.ui.userPage.UserManagementScreen
 
 import com.example.rentparkkotlin.ui.theme.RentParkKotlinTheme
@@ -45,7 +46,8 @@ class MainActivity : ComponentActivity() {
                     //DashboardScreen()
                     //RentParkHomeScreen()
                     //UserManagementScreen()
-                    CarListScreen()
+                    //CarListScreen()
+                    ContratsScreen()
                 }
             }
         }
