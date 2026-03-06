06d76e5f3d01172fd5552c21eca32972416281ba
clement roudier <clement.roudier@etu.uca.fr>
2026-03-06 08:31:13 +0100

    edit vehicule

--- RentParkKotlin/app/src/main/java/com/example/rentparkkotlin/MainActivity.kt
+++ RentParkKotlin/app/src/main/java/com/example/rentparkkotlin/MainActivity.kt
@@ -3,26 +3,15 @@ package com.example.rentparkkotlin
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import com.example.rentparkkotlin.ui.dashboard.DashboardScreen
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.rentparkkotlin.ui.carsPage.CarListScreen
-import com.example.rentparkkotlin.ui.homeCustomer.RentParkHomeScreen
-import com.example.rentparkkotlin.ui.reservationsPage.ContratsScreen
-import com.example.rentparkkotlin.ui.userPage.UserManagementScreen
-import com.example.rentparkkotlin.ui.settings.SettingsPage
+import com.example.rentparkkotlin.ui.contratsPage.ContratsScreen
 
 import com.example.rentparkkotlin.ui.theme.RentParkKotlinTheme
 import com.example.rentparkkotlin.ui.theme.ThemePrefs
