58d246b1264c238d7b0efb5c53a9524866e74db6
damien.gauthier2 <damien.gauthier2@etu.uca.fr>
2026-03-06 16:17:31 +0100

    add and delete cars android

--- RentParkKotlin/app/src/main/java/com/example/rentparkkotlin/data/ApiService.kt
+++ RentParkKotlin/app/src/main/java/com/example/rentparkkotlin/data/ApiService.kt
@@ -1,10 +1,20 @@
 package com.example.rentparkkotlin.data
 import com.example.rentparkkotlin.model.Voiture
+import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
 
 
 interface ApiService {
 
     @GET("voitures")
     suspend fun getVoitures(): List<Voiture>
+
+    @POST("vehicule")
+    suspend fun addVoiture(@Body newVoiture: Voiture) : retrofit2.Response<Unit>
+
+    @DELETE("delete/voitures/{id}")
+    suspend fun deleteVoiture(@Path("id") id: String): retrofit2.Response<Unit>
 }
