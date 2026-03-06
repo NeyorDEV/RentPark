7fd7b78acbbb4af0a076c4ab5ff4465d97a1893c
clement roudier <clement.roudier@etu.uca.fr>
2026-03-06 15:13:09 +0100

    fix requestException error in userController

--- RentParkKotlin/app/src/main/java/com/example/rentparkkotlin/MainActivity.kt
+++ RentParkKotlin/app/src/main/java/com/example/rentparkkotlin/MainActivity.kt
@@ -48,7 +48,7 @@ class MainActivity : ComponentActivity() {
                     //DashboardScreen()
                     //RentParkHomeScreen()
                     //PlanningScreen()
-                    UserManagementScreen()
+                    //UserManagementScreen()
                     //SettingsPage(
                     //    isDarkMode = isDarkMode,
                     //    onThemeChange = { newValue ->
