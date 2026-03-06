657e5a5403989702a6d6013b6bc4b4873078f94a
clement roudier <clement.roudier@etu.uca.fr>
2026-03-06 08:54:24 +0100

    update

--- RentParkKotlin/app/src/main/java/com/example/rentparkkotlin/MainActivity.kt
+++ RentParkKotlin/app/src/main/java/com/example/rentparkkotlin/MainActivity.kt
@@ -45,7 +45,7 @@ class MainActivity : ComponentActivity() {
                     //DashboardScreen()
                     //RentParkHomeScreen()
                     //PlanningScreen()
-                    //UserManagementScreen()
+                    UserManagementScreen()
                     //SettingsPage(
                     //    isDarkMode = isDarkMode,
                     //    onThemeChange = { newValue ->
