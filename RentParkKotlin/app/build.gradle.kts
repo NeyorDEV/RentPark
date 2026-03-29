54f867c7e0c86f6013e6c81bdd5e229037e6f9dd
clroudier <clement.roudier@etu.uca.fr>
2026-03-29 15:29:06 +0200

    userviewmodeltest

--- RentParkKotlin/app/build.gradle.kts
+++ RentParkKotlin/app/build.gradle.kts
@@ -82,4 +82,5 @@ dependencies {
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
     testImplementation("androidx.arch.core:core-testing:2.2.0")
     testImplementation("io.mockk:mockk:1.13.8")
+    testImplementation(kotlin("test"))
 }
