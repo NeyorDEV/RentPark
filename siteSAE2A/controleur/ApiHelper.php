d46870e571da195a714d0189a313e174d625e6da
qumiotto <quentin.miotto@etu.uca.fr>
2026-03-31 14:27:35 +0200

    getContratApiClient

--- siteSAE2A/controleur/ApiHelper.php
+++ siteSAE2A/controleur/ApiHelper.php
@@ -24,3 +24,10 @@ function getAuthApiClient(): Client
         'timeout'  => 30.0,
     ]);
 }
+function getContratApiClient(): Client
+{
+    return new Client([
+        'base_uri' => 'https://codefirst.iut.uca.fr/kubernetes/iut-inf63-projets-etudiants-rentpark/rentpark-contrat-pod/',
+        'timeout'  => 30.0,
+    ]);
+}
